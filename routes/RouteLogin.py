b802511cd0f78f4cb5f9e50fdd07c2f15437fcb7
Lulaley <remi.joulin@gmail.com>
2024-07-24 16:20:45 +0200

    fix import

--- routes/RouteLogin.py
+++ routes/RouteLogin.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-import ControleurLdap
+from ControleurLdap import ControleurLdap
 
 login = Blueprint('auth', __name__)
 
