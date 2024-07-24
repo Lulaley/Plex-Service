57cf61fb5ca7b44adda80d2da1ee20a9bc301dd7
Lulaley <remi.joulin@gmail.com>
2024-07-24 16:18:13 +0200

    fix import

--- static/Controleur/__init__.py
+++ static/Controleur/__init__.py
@@ -1,4 +1,4 @@
-from ControleurConf import ControleurConf
+import ControleurConf
 from ControleurLdap import ControleurLdap
 from ControleurLog import write_log
 from ControleurTorrent import download_torrent
