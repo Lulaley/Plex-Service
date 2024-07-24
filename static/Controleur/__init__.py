dce9647eb379c652de23e146cb7dfb84a546de07
Lulaley <remi.joulin@gmail.com>
2024-07-24 15:59:29 +0200

    fix import

--- static/Controleur/__init__.py
+++ static/Controleur/__init__.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/chemin/absolu/vers/Plex-Service/static')
+
 from ControleurConf import ControleurConf
 from ControleurLdap import ControleurLdap
 from ControleurLog import write_log
