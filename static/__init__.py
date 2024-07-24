ec08d3469d304cfd82d93f92a7702859f431f61b
Lulaley <remi.joulin@gmail.com>
2024-07-24 15:58:21 +0200

    fix import

--- static/__init__.py
+++ static/__init__.py
@@ -1,4 +1,8 @@
+import sys
+sys.path.append('/chemin/absolu/vers/Plex-Service/static')
+
 from Controleur.ControleurConf import ControleurConf
 from Controleur.ControleurLog import write_log
 from Controleur.ControleurLdap import ControleurLdap
 from Controleur.ControleurTorrent import download_torrent
+from Controleur.ControleurTorrent import send_download_info
