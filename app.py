07f54f21ec560d1e2cd6e2a5861bc91fc9d8b4d7
Lulaley <remi.joulin@gmail.com>
2024-07-25 17:07:49 +0200

    removes Blueprint

--- app.py
+++ app.py
@@ -3,21 +3,17 @@ import sys
 chemin_actuel = os.path.dirname(__file__)
 chemin_routes = os.path.join(chemin_actuel, '../routes')
 sys.path.append(chemin_routes)
-
-from flask import Flask, Blueprint
-
+from flask import Flask
 from routes.RouteLogin import login
-# Importez d'autres Blueprints ici
 
 app = Flask(__name__)
 
-login = Blueprint('login', __name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@login.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login_page():
     # Votre logique de traitement ici
     return 'Page de connexion'
