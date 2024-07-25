aed99fd5778ab74954ef5a2469d11f4d33276166
Lulaley <remi.joulin@gmail.com>
2024-07-25 17:04:22 +0200

    new code for controlleur torrent

--- static/Controleur/ControleurTorrent.py
+++ static/Controleur/ControleurTorrent.py
@@ -1,33 +1,31 @@
 import time
 import requests
-from .ControleurLog import write_log
-import pybittorrent as BTLT
+from PyBitTorrent import TorrentClient
+from static.Controleur.ControleurLog import write_log
 
 def download_torrent(torrent_file_path, save_path):
-    ses = BTLT.Session()
-    h = ses.add_torrent(torrent_file_path, save_path)
+    # Création d'une instance de TorrentClient
+    client = TorrentClient()
 
-    write_log('Téléchargement en cours : {}'.format(h.name()))
-    while not h.is_seed():
-        s = h.status()
-        write_log('%.2f%% complet (téléchargement : %.1f kB/s, envoi : %.1f kB/s, pairs : %d) %s' % (
-            s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
-            s.num_peers, s.state))
-        send_download_info(h.name(), s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, s.state)
+    # Ajout du torrent et définition du chemin de sauvegarde
+    torrent = client.add_torrent(torrent_file_path, save_path)
+
+    write_log('Téléchargement en cours : {}'.format(torrent.name))
+    while not torrent.is_finished:
+        status = torrent.status()
+        write_log('%.2f%% complet (téléchargement : %.1f kB/s, envoi : %.1f kB/s, pairs : %d)' % (
+            status.progress * 100, status.download_rate / 1000, status.upload_rate / 1000,
+            status.num_peers))
+        send_download_info(torrent.name, status.progress * 100, status.download_rate / 1000, status.upload_rate / 1000, status.num_peers, status.state)
         time.sleep(1)
 
-    write_log('{} terminé'.format(h.name()))
+    write_log('{} terminé'.format(torrent.name))
 
-    ses.remove_torrent(h)  # Stop seeding the torrent
+    # Suppression du torrent de la session pour arrêter le partage
+    client.remove_torrent(torrent)
 
-# Exemple d'utilisation
-torrent_file_path = '/chemin/vers/votre/fichier.torrent'
-save_path = '/chemin/vers/votre/dossier/de/destination'
-download_torrent(torrent_file_path, save_path)
-
-# Fonction pour envoyer les informations de téléchargement à l'application Flask
 def send_download_info(name, progress, download_rate, upload_rate, num_peers, state):
-    url = 'http://localhost:5000/download_info'  # Remplacez par l'URL de votre application Flask
+    url = 'http://localhost:5000/download_info'
     data = {
         'name': name,
         'progress': progress,
