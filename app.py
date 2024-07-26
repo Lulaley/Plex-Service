56bd94a8bd0d5ff7855d0f08fedf1dc451a524ad
Lulaley <remi.joulin@gmail.com>
2024-07-26 08:59:46 +0200

    add route for .html

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 chemin_actuel = os.path.dirname(__file__)
 chemin_routes = os.path.join(chemin_actuel, '../routes')
 sys.path.append(chemin_routes)
-from flask import Flask
+from flask import Flask, render_template
 from routes.RouteLogin import login
 
 app = Flask(__name__)
@@ -11,12 +11,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    # Votre logique de traitement ici
-    return 'Page de connexion'
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='0.0.0.0')
