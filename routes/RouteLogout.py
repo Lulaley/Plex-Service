e241f56837f5818ddb273aabf3339c3513704976
Lulaley <remi.joulin@gmail.com>
2024-07-29 14:32:23 +0200

    fix logout

--- routes/RouteLogout.py
+++ routes/RouteLogout.py
@@ -1,4 +1,4 @@
-from flask import session, render_template
+from flask import session, redirect, url_for
 
 def logout(app):
     @app.route('/logout')
@@ -6,4 +6,4 @@ def logout(app):
         # Supprimer les informations de session de l'utilisateur
         session.pop('username', None)
         # Rediriger vers la page d'accueil
-        return render_template('index.html')
+        return redirect(url_for('index'))
