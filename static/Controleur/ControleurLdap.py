d74f942d1ba33113129c096b6e4dcd7af457f132
Lulaley <remi.joulin@gmail.com>
2024-07-29 11:00:01 +0200

    changement de lib ldap

--- static/Controleur/ControleurLdap.py
+++ static/Controleur/ControleurLdap.py
@@ -1,14 +1,14 @@
-import ldap
 import hashlib
 import os
 import base64
 from .ControleurConf import ControleurConf
+import pyldap
 
 class ControleurLdap:
     def __init__(self):
         self.config = ControleurConf()
         self.server = self.config.get_config('LDAP', 'server')
-        self.conn = ldap.initialize(self.server)
+        self.conn = pyldap.initialize(self.server)
 
     def bind_as_root(self):
         try:
@@ -17,7 +17,7 @@ class ControleurLdap:
             password_ssha = self.convert_to_ssha(root_password)
             self.conn.simple_bind_s(root_dn, password_ssha)
             print("Connexion en tant que root réussie")
-        except ldap.LDAPError as e:
+        except pyldap.LDAPError as e:
             print("Erreur de connexion en tant que root:", e)
 
     def convert_to_ssha(self, password):
@@ -40,7 +40,7 @@ class ControleurLdap:
             else:
                 print("Utilisateur non trouvé")
                 return False
-        except ldap.LDAPError as e:
+        except pyldap.LDAPError as e:
             print("Erreur d'authentification:", e)
             return False
 
@@ -48,52 +48,52 @@ class ControleurLdap:
         try:
             search_base = self.config.get_config('LDAP', 'base_dn')
             search_filter = f"(uid={username})"
-            result = self.conn.search_s(search_base, ldap.SCOPE_SUBTREE, search_filter)
+            result = self.conn.search_s(search_base, pyldap.SCOPE_SUBTREE, search_filter)
             if result:
                 print("Utilisateur trouvé")
                 return result[0][0]
             else:
                 print("Utilisateur non trouvé")
                 return None
-        except ldap.LDAPError as e:
+        except pyldap.LDAPError as e:
             print("Erreur lors de la recherche de l'utilisateur:", e)
 
     def add_entry(self, dn, attributes):
         try:
             self.conn.add_s(dn, attributes)
             print("Entrée ajoutée avec succès")
-        except ldap.LDAPError as e:
+        except pyldap.LDAPError as e:
             print("Erreur lors de l'ajout de l'entrée:", e)
 
     def delete_entry(self, dn):
         try:
             self.conn.delete_s(dn)
             print("Entrée supprimée avec succès")
-        except ldap.LDAPError as e:
+        except pyldap.LDAPError as e:
             print("Erreur lors de la suppression de l'entrée:", e)
 
     def modify_entry(self, dn, mod_list):
         try:
             self.conn.modify_s(dn, mod_list)
             print("Entrée modifiée avec succès")
-        except ldap.LDAPError as e:
+        except pyldap.LDAPError as e:
             print("Erreur lors de la modification de l'entrée:", e)
 
     def search_entry(self, search_base, search_filter):
         try:
-            result = self.conn.search_s(search_base, ldap.SCOPE_SUBTREE, search_filter)
+            result = self.conn.search_s(search_base, pyldap.SCOPE_SUBTREE, search_filter)
             if result:
                 print("Entrée trouvée")
                 return result
             else:
                 print("Entrée non trouvée")
                 return None
-        except ldap.LDAPError as e:
+        except pyldap.LDAPError as e:
             print("Erreur lors de la recherche de l'entrée:", e)
     
     def disconnect(self):
         try:
             self.conn.unbind_s()
             print("Déconnexion LDAP réussie")
-        except ldap.LDAPError as e:
+        except pyldap.LDAPError as e:
             print("Erreur lors de la déconnexion LDAP:", e)
