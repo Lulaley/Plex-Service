a81e75fcc3f071ea1bb3f0908c86a7df465871cf
Lulaley <remi.joulin@gmail.com>
2024-07-30 14:48:12 +0200

    chore: Add error logging and flash message for user registration failure in RouteRegister

--- routes/RouteRegister.py
+++ routes/RouteRegister.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for, flash, session
 from static.Controleur.ControleurLdap import ControleurLdap
 from static.Controleur.ControleurConf import ControleurConf
+from static.Controleur.ControleurLog import write_log
 
 def register(app):
     @app.route('/register', methods=['GET', 'POST'])
@@ -25,9 +26,17 @@ def register(app):
             ]
 
             # Créer une instance de ControleurLdap et ajouter l'utilisateur
-            ds = ControleurLdap()
-            ds.add_entry(dn, attributes)
-            ds.disconnect()
+            while True:
+                ds = ControleurLdap()
+                try:
+                    ds.add_entry(dn, attributes)
+                    ds.disconnect()
+                    break
+                except Exception as e:
+                    write_log(f"Erreur lors de l'ajout de l'utilisateur: {str(e)}")
+                    flash('Erreur lors de l\'ajout de l\'utilisateur. Veuillez réessayer.')
+                    ds.disconnect()
+                    return render_template('register.html')
             session['username'] = username
             return redirect(url_for('home'))
 
