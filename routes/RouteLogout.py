6fcffcdb0871c4dd457038f55b2545395550287d
Lulaley <remi.joulin@gmail.com>
2024-07-31 10:08:01 +0200

    chore: Update logout route to write user logout information to log file

--- routes/RouteLogout.py
+++ routes/RouteLogout.py
@@ -1,9 +1,11 @@
 from flask import session, redirect, url_for
+from static.Controleur.ControleurLog import write_log
 
 def logout(app):
     @app.route('/logout')
     def inner_logout():
         # Supprimer la session de l'utilisateur
+        write_log(f"Utilisateur déconnecté: {session['username']}")
         session.pop('username', None)
         # Rediriger vers la route racine
         return redirect(url_for('index'))
