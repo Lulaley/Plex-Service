52ba79a557faa2629b7dfe7b4146d2f15ba3b06c
BlazeOne <yann.tsatsaris@gmail.com>
2024-08-01 20:52:22 +0200

    chore: Update RouteRegister.py to hash user password during registration

--- routes/RouteRegister.py
+++ routes/RouteRegister.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for, flash, session
 from static.Controleur.ControleurLdap import ControleurLdap
 from static.Controleur.ControleurConf import ControleurConf
 from static.Controleur.ControleurLog import write_log
+from passlib.hash import ldap_salted_sha1
 
 def register(app):
     @app.route('/register', methods=['GET', 'POST'])
@@ -11,6 +12,7 @@ def register(app):
             password = request.form['createPassword']
             email = request.form['email']
 
+            hashed_password = ldap_salted_sha1.hash(password)
             conf = ControleurConf()
             base_dn = conf.get_config('LDAP', 'base_dn')
             ds = ControleurLdap()
@@ -26,7 +28,7 @@ def register(app):
                 ('uid', [username.encode('utf-8')]),
                 ('sn', [username.encode('utf-8')]),  # Nom de famille
                 ('cn', [username.encode('utf-8')]),  # Nom complet
-                ('userPassword', [password.encode('utf-8')]),  # Mot de passe en clair
+                ('userPassword', [hashed_password.encode('utf-8')]),  # Mot de passe en clair
                 ('mail', [email.encode('utf-8')])
             ]
 
