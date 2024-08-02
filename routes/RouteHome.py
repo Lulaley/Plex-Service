8e512139f0303d68699e0b70a4b663947e14df04
Lulaley <remi.joulin@gmail.com>
2024-08-02 11:02:52 +0200

    Refactor RouteHome.py for improved user rights verification

--- routes/RouteHome.py
+++ routes/RouteHome.py
@@ -8,9 +8,7 @@ def get_user_rights(username):
     conf = ControleurConf()
     write_log(f"Recherche des droits pour l'utilisateur: {username}")
     res = ds.search_user(username)
-    write_log(f"Résultat de la recherche: {res}")
-    if res:
-        write_log(f"Utilisateur {username} trouvé dans LDAP")
+    if ds.search_user(username):
         user_dn = f"(uid={username})"
         search_base = conf.get_config('LDAP', 'base_dn')
         user_entry = ds.search_entry(search_base, user_dn)
