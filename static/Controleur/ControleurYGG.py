90f57599fc3effa97cacb79d8e34642e12455357
BlazeOne <yann.tsatsaris@gmail.com>
2024-08-03 18:30:52 +0200

    chore: Update Chrome binary location and disable GPU in YGG controller

--- static/Controleur/ControleurYGG.py
+++ static/Controleur/ControleurYGG.py
@@ -16,7 +16,7 @@ class ControleurYGG:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         # chrome_options.add_argument("--headless")  # Désactiver le mode headless
-        chrome_options.add_argument("--disable-gpu")
+        # chrome_options.add_argument("--disable-gpu")  # Essayez de désactiver cette option
         chrome_options.add_argument("--remote-debugging-port=9222")
         
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
