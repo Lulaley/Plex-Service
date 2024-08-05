3159646fd188142bf96cce069e2f7e55794561e6
Lulaley <remi.joulin@gmail.com>
2024-08-05 10:40:25 +0200

    chore: Update ControleurTorrent.py to handle OutOfPeers exception during torrent download

--- static/Controleur/ControleurTorrent.py
+++ static/Controleur/ControleurTorrent.py
@@ -1,6 +1,7 @@
 import time
 import requests
 from PyBitTorrent import TorrentClient
+from PyBitTorrent.Exceptions import OutOfPeers
 from static.Controleur.ControleurLog import write_log
 
 # Variable globale pour stocker l'état du téléchargement
@@ -11,34 +12,26 @@ def download_torrent(torrent_file_path, save_path):
     # Création d'une instance de TorrentClient avec l'argument requis
     torrent = TorrentClient(torrent_file_path, output_dir=save_path)
 
-    torrent.start()
-    # Ajout du torrent et définition du chemin de sauvegarde
-    #torrent = client.add_torrent(torrent_file_path, save_path)
-
-    write_log('Téléchargement en cours : {}'.format(torrent.name))
-    while not torrent.is_done():
-        status = torrent.status()
-        write_log('%.2f%% complet (téléchargement : %.1f kB/s, envoi : %.1f kB/s, pairs : %d)' % (
-            status.progress * 100, status.download_rate / 1000, status.upload_rate / 1000,
-            status.num_peers))
-        send_download_info(torrent.name, status.progress * 100, status.download_rate / 1000, status.upload_rate / 1000, status.num_peers, status.state)
+    try:
+        status = torrent.start()
+        write_log(f'Téléchargement du torrent {status} démarré')
         
-        # Mise à jour de l'état du téléchargement
+
+        #write_log('{} terminé'.format(torrent.name))
+
+        # Suppression du torrent de la session pour arrêter le partage
+        torrent.remove()
+
+    except OutOfPeers:
+        write_log('Erreur : Plus de pairs disponibles pour le téléchargement du torrent.')
         download_status = {
-            'name': torrent.name,
-            'progress': status.progress * 100,
-            'download_rate': status.download_rate / 1000,
-            'upload_rate': status.upload_rate / 1000,
-            'num_peers': status.num_peers,
-            'state': status.state
+            'name': torrent_file_path,
+            'progress': 0,
+            'download_rate': 0,
+            'upload_rate': 0,
+            'num_peers': 0,
+            'state': 'Erreur : Plus de pairs disponibles'
         }
-        
-        time.sleep(1)
-
-    write_log('{} terminé'.format(torrent.name))
-
-    # Suppression du torrent de la session pour arrêter le partage
-    torrent.remove()
 
     # Réinitialisation de l'état du téléchargement
     download_status = {}
