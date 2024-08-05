9230b2d582f78b632b26625d6244015a713392e7
Lulaley <remi.joulin@gmail.com>
2024-08-05 15:34:30 +0200

    Improve torrent download functionality and add logging

--- static/Controleur/ControleurTorrent.py
+++ static/Controleur/ControleurTorrent.py
@@ -7,17 +7,23 @@ download_status = {}
 
 def download_torrent(torrent_file_path, save_path):
 
+    write_log(f"Ouverture de la session de téléchargement pour {torrent_file_path}")
     ses = lt.session()
 
+    write_log(f"Chargement du fichier .torrent pour {torrent_file_path}")
     info = lt.torrent_info(torrent_file_path)  # replace with your torrent file
+    write_log(f"Chargement du fichier .torrent pour {torrent_file_path} terminé")
     h = ses.add_torrent({'ti': info, 'save_path': save_path})  # download to current directory
+    write_log(f"Téléchargement du fichier .torrent pour {torrent_file_path} démarré")
 
-    print('downloading', info.name())
+    write_log('downloading', info.name())
     while not h.is_seed():
         s = h.status()
-        print('%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
+        write_log('%.2f%% complete (down: %.1f kB/s up: %.1f kB/s peers: %d) %s' % (
             s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000,
             s.num_peers, s.state))
         time.sleep(1)
 
-    print(info.name(), 'complete')
+    write_log(info.name(), 'complete')
+    write_log(f"Téléchargement du fichier .torrent pour {torrent_file_path} terminé")
+    ses.remove_torrent(h)
