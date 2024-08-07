1fd632462fbe2af8e864b6a1b803e0c1c1d09b58
Lulaley <remi.joulin@gmail.com>
2024-08-07 13:36:17 +0200

    Refactor RouteDownload.py to remove unnecessary code and improve error handling

--- routes/RouteDownload.py
+++ routes/RouteDownload.py
@@ -58,7 +58,7 @@ def start_download(app):
             write_log(f"Téléchargement du fichier .torrent pour {username}")
             #download_thread = threading.Thread(target=download_torrent, args=(torrent_file_path, save_path))
             #download_thread.start()
-            download_torrent(torrent_file_path, save_path)
+            download_torrent(torrent_file_path)
         except:
             write_log(f"Erreur lors du téléchargement du fichier .torrent pour {username}")
             flash('Erreur lors du téléchargement du fichier .torrent')
