bf48099d0a7cfa07aba3aa7bc0317a9cf9eff349
BlazeOne <yann.tsatsaris@gmail.com>
2025-02-25 10:38:47 +0100

    Ajout de nouveaux attributs et classes d'objet dans le schéma LDAP pour gérer les demandes de films des utilisateurs.

--- routes/RouteRegister.py
+++ routes/RouteRegister.py
@@ -36,24 +36,24 @@ def register(app):
                 return redirect(url_for('index'))
             write_log(f"L'utilisateur {username} n'existe pas, ajout en cours")
             # Construire le DN et les attributs de l'utilisateur
-#            dn = f"uid={username},dmdName=users,{base_dn}"
-#            attributes = [
-#                ('objectClass', [b'inetOrgPerson', b'organizationalPerson', b'person']),
-#                ('uid', [username.encode('utf-8')]),
-#                ('sn', [username.encode('utf-8')]),  # Nom de famille
-#                ('cn', [username.encode('utf-8')]),  # Nom complet
-#                ('userPassword', [hashed_password.encode('utf-8')]),  # Mot de passe en clair
-#                ('mail', [email.encode('utf-8')])
-#            ]
+            dn = f"uid={username},dmdName=users,{base_dn}"
+            attributes = [
+                ('objectClass', [b'inetOrgPerson', b'organizationalPerson', b'person']),
+                ('uid', [username.encode('utf-8')]),
+                ('sn', [username.encode('utf-8')]),  # Nom de famille
+                ('cn', [username.encode('utf-8')]),  # Nom complet
+                ('userPassword', [hashed_password.encode('utf-8')]),  # Mot de passe en clair
+                ('mail', [email.encode('utf-8')])
+            ]
 
             # Créer une instance de ControleurLdap et ajouter l'utilisateur
             userAdd = False
-#            if ds.add_entry(dn, attributes):
-#                write_log(f"Utilisateur ajouté: {username}")
-#                userAdd = True
-#            else:
-#                write_log(f"Erreur lors de l'ajout de l'utilisateur: {username}", 'ERROR')
-#            ds.disconnect()
+            if ds.add_entry(dn, attributes):
+                write_log(f"Utilisateur ajouté: {username}")
+                userAdd = True
+            else:
+                write_log(f"Erreur lors de l'ajout de l'utilisateur: {username}", 'ERROR')
+                ds.disconnect()
             if userAdd:
                 session['username'] = username
                 return redirect(url_for('home'))
