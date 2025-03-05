511399f1fc94375dc8ff4a993ac03706dddcf00b
BlazeOne <yann.tsatsaris@gmail.com>
2025-03-05 18:07:02 +0100

    Ajout de la prise en charge de la langue française pour les recherches de séries et de films dans TMDB

--- static/Controleur/ControleurTMDB.py
+++ static/Controleur/ControleurTMDB.py
@@ -7,27 +7,27 @@ class ControleurTMDB:
         conf = ControleurConf()
         tmdb.API_KEY = conf.get_config('TMDB', 'api_key')
         tmdb.REQUESTS_TIMEOUT = (2, 5)  # seconds, for connect and read specifically
-        
+
     def search_serie(self, title):
         search = tmdb.Search()
         write_log(f"Recherche de la série {title} dans la base de données TMDB")
-        search.tv(query=title)
+        search.tv(query=title, language='fr-FR')
         return search.results[0]
-    
+
     def search_movie(self, title):
         search = tmdb.Search()
         write_log(f"Recherche du film {title} dans la base de données TMDB")
-        search.movie(query=title)
+        search.movie(query=title, language='fr-FR')
         return search.results[0]
 
     def search_all_series(self, title):
         search = tmdb.Search()
         write_log(f"Recherche de toutes les séries correspondant à {title} dans la base de données TMDB")
-        response = search.tv(query=title)
+        response = search.tv(query=title, language='fr-FR')
         return response['results']
-    
+
     def search_all_movies(self, title):
         search = tmdb.Search()
         write_log(f"Recherche de tous les films correspondant à {title} dans la base de données TMDB")
-        response = search.movie(query=title)
+        response = search.movie(query=title, language='fr-FR')
         return response['results']
